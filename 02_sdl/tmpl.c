32e02074b3305dbdfd05b00aca2f1fa8d07ef76b
Andrei Rabusov <a.rabusov@tum.de>
2023-04-04 17:39:04 +0200

    [SDL] add template

---
+++ 02_sdl/tmpl.c
@@ -0,0 +1,83 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SCREEN_WIDTH    1024
+#define SCREEN_HEIGHT   748
+
+typedef struct {
+        SDL_Renderer    *renderer;
+        SDL_Window      *window;
+} App;
+
+App app;
+
+void initSDL(void)
+{
+        int rendererFlags, windowFlags;
+
+        rendererFlags = SDL_RENDERER_ACCELERATED;
+
+        windowFlags = 0;
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+                printf("Can't initialize SDL: %s\n", SDL_GetError());
+                exit(1);
+        }
+
+        app.window = SDL_CreateWindow("Str 01", SDL_WINDOWPOS_UNDEFINED,
+                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
+                        windowFlags);
+
+        if (!app.window) {
+                printf("failed to open window\n");
+                exit(1);
+        }
+
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+
+        app.renderer = SDL_CreateRenderer(app.window, -1, rendererFlags);
+
+        if (!app.renderer) {
+                printf("stop.\n");
+                exit(1);
+        }
+}
+
+void doInput(void)
+{
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+                switch (event.type) {
+                        case SDL_QUIT: exit(0);
+                        default: break;
+                }
+        }
+}
+
+void prepareScene(void)
+{
+        SDL_SetRenderDrawColor(app.renderer, 96, 128,255, 255);
+        SDL_RenderClear(app.renderer);
+}
+
+void presentScene(void)
+{
+        SDL_RenderPresent(app.renderer);
+}
+
+int main(void)
+{
+        memset(&app, 0, sizeof(app));
+
+        initSDL();
+
+        while(1) {
+                prepareScene();
+                doInput();
+                presentScene();
+                SDL_Delay(16);
+        }
+
+        return 0;
+}
+

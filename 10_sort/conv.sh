047848738e537a08775920782e7b7413d3d1db94
Andrei Rabusov <a.rabusov@tum.de>
2023-06-26 22:00:33 +0200

    [pas] add converter for MS-DOS

---
+++ 10_sort/conv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+INPUT=$1
+if [[ ! -f $INPUT ]]; then
+    echo "Usage: conv.sh INPUT_FILE"
+    exit 1
+fi
+OUTPUT=${INPUT^^}
+echo '{ DOSBox configuration. Type before running TPX:' > $OUTPUT
+echo '      Z:\KEYB DE 866' >> $OUTPUT
+echo '}' >> $OUTPUT
+iconv -f=utf-8 -t=CP866 $INPUT | unix2dos >> $OUTPUT

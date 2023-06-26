65a60be1e681065705a2d293e557deb99eb9c315
Andrei Rabusov <a.rabusov@tum.de>
2023-06-26 22:14:35 +0200

    [pas] correct the comment in the script

--- 10_sort/conv.sh
+++ 10_sort/conv.sh
@@ -7,6 +7,6 @@ if [[ ! -f $INPUT ]]; then
 fi
 OUTPUT=${INPUT^^}
 echo '{ DOSBox configuration. Type before running TPX:' > $OUTPUT
-echo '      Z:\KEYB DE 866' >> $OUTPUT
+echo '      Z:\KEYB ru441 866' >> $OUTPUT
 echo '}' >> $OUTPUT
 iconv -f=utf-8 -t=CP866 $INPUT | unix2dos >> $OUTPUT
